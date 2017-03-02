21b106c4482240fb956563f1b96cf5f51e276cab
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2017-03-02 12:10:52 +0000

    Refactor: extract explaining variables

--- src/solution/e74.clj
+++ src/solution/e74.clj
@@ -1,4 +1,6 @@
 (fn [xs]
   (letfn [(perfect-square [n] (= (Math/pow (int (Math/sqrt n)) 2) (double n)))
-            (parse-int [s] (Integer/parseInt (re-find #"\A-?\d+" s)))] 
-  (clojure.string/join "," (filter perfect-square (map parse-int (clojure.string/split xs #","))))))
+          (parse-int [s] (Integer/parseInt (re-find #"\A-?\d+" s)))
+          (get-values [xs] (map parse-int (clojure.string/split xs #",")))
+          (format-values [values] (clojure.string/join "," values))] 
+  (format-values (filter perfect-square (get-values xs)))))
