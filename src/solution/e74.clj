0dd0946984fbf61312215411e6a30616413b0907
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2017-03-02 12:12:33 +0000

    Refactor: prefer using the thread function

--- src/solution/e74.clj
+++ src/solution/e74.clj
@@ -2,5 +2,8 @@
   (letfn [(perfect-square [n] (= (Math/pow (int (Math/sqrt n)) 2) (double n)))
           (parse-int [s] (Integer/parseInt (re-find #"\A-?\d+" s)))
           (get-values [xs] (map parse-int (clojure.string/split xs #",")))
-          (format-values [values] (clojure.string/join "," values))] 
-  (format-values (filter perfect-square (get-values xs)))))
+          (format-values [values] (clojure.string/join "," values))]
+    (->> xs
+         get-values
+         (filter perfect-square)
+         format-values)))
