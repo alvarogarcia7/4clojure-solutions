0918cc1ff35e61e440077ead85022200fdd2d843
Alvaro Garcia <derecerca@gmail.com>
2017-03-08 18:31:58 +0000

    Refactor: extract explaining function

--- src/solution/e43.clj
+++ src/solution/e43.clj
@@ -2,7 +2,8 @@
   [coll n] 
   (let [dest (into [] (->> (range n) (map (fn [_] []))))
         in-which-array (map #(mod % n) (range (count coll)))
-        merged (mapv #(-> [%1 %2]) coll in-which-array)]
-  (reduce (fn [acc [val ind]]
-    (let [added (conj (nth acc ind) val)]
-      (assoc acc ind added))) dest merged)))
+        merged (mapv #(-> [%1 %2]) coll in-which-array)
+        add-at-position (fn [acc ind val] 
+          (assoc acc ind (conj (nth acc ind) val)))]
+  (reduce (fn [acc [val ind]] 
+      (add-at-position acc ind val)) dest merged)))
