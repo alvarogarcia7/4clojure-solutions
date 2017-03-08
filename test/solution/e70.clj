ed050ae4cf4040f1d5e65faa8a95c8030d9be72f
Alvaro Garcia <derecerca@gmail.com>
2017-03-08 07:04:02 +0000

    Create structure to execute tests

--- test/solution/e70.clj
+++ test/solution/e70.clj
@@ -1,8 +1,19 @@
-(= (__  "Have a nice day.")
-   ["a" "day" "Have" "nice"])
+(ns solution.e70
+   (:require [clojure.test :refer :all]))
 
-(= (__  "Clojure is a fun language!")
-   ["a" "Clojure" "fun" "is" "language"])
+(defn iss [& forms]
+   (doall (map #(is %) forms)))
 
-(= (__  "Fools fall for foolish follies.")
-   ["fall" "follies" "foolish" "Fools" "for"])
+(defn __ [args] ((load-file "src/solution/e70.clj") args))
+
+(deftest canary (= true true))
+
+(iss
+   (= (__ "Have a nice day.")
+      ["a" "day" "Have" "nice"])
+
+   (= (__ "Clojure is a fun language!")
+      ["a" "Clojure" "fun" "is" "language"])
+
+   (= (__ "Fools fall for foolish follies.")
+      ["fall" "follies" "foolish" "Fools" "for"]))
