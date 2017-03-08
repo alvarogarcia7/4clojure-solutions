baba5a3815fe15e281143a5fe717de1d64598f2c
Alvaro Garcia <derecerca@gmail.com>
2017-03-08 18:28:07 +0000

    Green: solve exercise 43

---
+++ src/solution/e43.clj
@@ -0,0 +1,8 @@
+(fn
+  [coll n] 
+  (let [dest (into [] (->> (range n) (map (fn [_] []))))
+        in-which-array (map #(mod % n) (range (count coll)))
+        merged (into [] (mapv #(-> [%1 %2]) coll in-which-array))]
+  (reduce (fn [acc [val ind]]
+    (let [added (conj (nth acc ind) val)]
+      (assoc acc ind added))) dest merged)))
