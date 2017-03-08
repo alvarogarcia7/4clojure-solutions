cc9bccaf01e012b5eb5169d417c02f34586b5f23
Alvaro Garcia <derecerca@gmail.com>
2017-03-08 06:09:17 +0000

    Refactor: convert to a thrust expression

--- src/solution/e77.clj
+++ src/solution/e77.clj
@@ -2,4 +2,8 @@
     (letfn [(anagram? [word n] (= (frequencies word) (frequencies n)))
             (only-anagrams [coll] (filter (fn [set] (> (count set) 1)) coll))
             (generate-equivalences [coll] (map (fn [word] (filter #(anagram? word %) coll)) coll))]
-      (into #{} (map #(into #{} %) (only-anagrams (generate-equivalences initial))))))
+      (->> initial
+           generate-equivalences
+           only-anagrams
+           (map #(into #{} %))
+           (into #{}))))
