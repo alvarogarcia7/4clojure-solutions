0529cd6f1c2a91bc8e78f34c6d9986cad1e5497d
Alvaro Garcia <derecerca@gmail.com>
2017-03-08 06:01:24 +0000

    Refactor: extract explaining function

--- src/solution/e77.clj
+++ src/solution/e77.clj
@@ -1,2 +1,7 @@
 (fn [initial]
-  (into #{} (map (fn [coll] (into #{} coll)) (filter (fn [coll] (> (count coll) 1)) (map (fn [word] (filter (fn [n] (= (frequencies word) (frequencies n))) initial)) initial)))))
+  (letfn [(anagram? [word n](= (frequencies word) (frequencies n)))
+          (only-anagrams [coll] (filter (fn [coll] (> (count coll) 1)) coll))
+          (generate-equivalences [coll] (map (fn [word] (filter
+                             (fn [word-] (anagram?
+                               word word-)) coll)) coll))]
+    (into #{} (map (fn [coll] (into #{} coll)) (only-anagrams (generate-equivalences initial))))))
