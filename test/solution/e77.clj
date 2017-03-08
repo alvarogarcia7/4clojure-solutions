bc66462f4445861f90ef2d7545f84e711e8a24db
Alvaro Garcia <derecerca@gmail.com>
2017-03-08 07:14:32 +0000

    Execute tests in e77

--- test/solution/e77.clj
+++ test/solution/e77.clj
@@ -1,5 +1,11 @@
-(= (__ ["meat" "mat" "team" "mate" "eat"])
-   #{#{"meat" "team" "mate"}})
+(ns solution.e77
+   (:require [solution.common :refer :all]))
 
-(= (__ ["veer" "lake" "item" "kale" "mite" "ever"])
-   #{#{"veer" "ever"} #{"lake" "kale"} #{"mite" "item"}})
+(def __ (read-source-file "e77"))
+
+(iss
+   (= (__ ["meat" "mat" "team" "mate" "eat"])
+      #{#{"meat" "team" "mate"}})
+
+   (= (__ ["veer" "lake" "item" "kale" "mite" "ever"])
+      #{#{"veer" "ever"} #{"lake" "kale"} #{"mite" "item"}}))
