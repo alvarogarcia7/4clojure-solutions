6df43622e27f667ee8cc94ad99c23cd46e307882
Alvaro Garcia <derecerca@gmail.com>
2017-03-08 06:05:58 +0000

    Refactor: prefer inline syntax for this

--- src/solution/e77.clj
+++ src/solution/e77.clj
@@ -1,8 +1,5 @@
   (fn [initial]
     (letfn [(anagram? [word n] (= (frequencies word) (frequencies n)))
             (only-anagrams [coll] (filter (fn [coll] (> (count coll) 1)) coll))
-            (generate-equivalences [coll] (map (fn [word] (filter
-                                                            (fn
-                                                              [word-]
-                                                              (anagram? word word-)) coll)) coll))]
+            (generate-equivalences [coll] (map (fn [word] (filter #(anagram? word %) coll)) coll))]
       (into #{} (map #(into #{} %) (only-anagrams (generate-equivalences initial))))))
