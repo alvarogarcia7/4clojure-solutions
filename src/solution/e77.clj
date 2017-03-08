9948f8602cf5baf03dd713077385facbd83cdfa3
Alvaro Garcia <derecerca@gmail.com>
2017-03-08 06:19:24 +0000

    Refactor: whitespace

--- src/solution/e77.clj
+++ src/solution/e77.clj
@@ -1,9 +1,9 @@
-  (fn [initial]
-    (letfn [(anagram? [word n] (= (frequencies word) (frequencies n)))
-            (only-anagrams [coll] (filter (fn [set] (> (count set) 1)) coll))
-            (group-by-same-composition [coll] (map (fn [word] (filter #(anagram? word %) coll)) coll))]
-      (->> initial
-           group-by-same-composition
-           only-anagrams
-           (map #(into #{} %))
-           (into #{}))))
+(fn [initial]
+  (letfn [(anagram? [word n] (= (frequencies word) (frequencies n)))
+          (only-anagrams [coll] (filter (fn [set] (> (count set) 1)) coll))
+          (group-by-same-composition [coll] (map (fn [word] (filter #(anagram? word %) coll)) coll))]
+    (->> initial
+         group-by-same-composition
+         only-anagrams
+         (map #(into #{} %))
+         (into #{}))))
