396b931a49705c094e425e0af8423d8a5b8d6c2f
Alvaro Garcia <derecerca@gmail.com>
2017-03-08 18:28:56 +0000

    Refactor: no need to convert to an array first

--- src/solution/e43.clj
+++ src/solution/e43.clj
@@ -2,7 +2,7 @@
   [coll n] 
   (let [dest (into [] (->> (range n) (map (fn [_] []))))
         in-which-array (map #(mod % n) (range (count coll)))
-        merged (into [] (mapv #(-> [%1 %2]) coll in-which-array))]
+        merged (mapv #(-> [%1 %2]) coll in-which-array)]
   (reduce (fn [acc [val ind]]
     (let [added (conj (nth acc ind) val)]
       (assoc acc ind added))) dest merged)))
