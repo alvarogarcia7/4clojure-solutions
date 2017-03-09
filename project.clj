64b6fcb7292fafaae1c549abf3c019ec6a3fc682
Alvaro Garcia <derecerca@gmail.com>
2017-03-09 19:34:00 +0000

    Add project with dependencies

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject fourclojure-solutions "0.0.1-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "FIXME choose a license"
+            :url ""}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.cli "0.3.3"]]
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}
+             }
+  )
