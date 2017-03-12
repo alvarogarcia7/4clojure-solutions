4c15ba3084286856db210458226eb7a42683005e
Alvaro Garcia <derecerca@gmail.com>
2017-03-12 19:37:00 +0000

    Add makefile

--- project.clj
+++ project.clj
@@ -6,6 +6,5 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.3.3"]]
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}
-             }
+  :profiles {:uberjar {:aot :all}}
   )
