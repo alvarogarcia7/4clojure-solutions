7b89b2ae373dc3032deaf9208f6d9528ce047e93
Alvaro Garcia <derecerca@gmail.com>
2017-03-21 07:31:17 +0000

    Refactor: remove dead code

--- src/solution/e56.clj
+++ src/solution/e56.clj
@@ -1,2 +1,1 @@
-;(fn [coll] (reduce (fn [acc ele] (if (contains? acc ele) acc (conj acc ele))) [] coll))
 (fn [coll] (reduce (fn [acc ele] (if (nil? (some #{ele} acc)) (conj acc ele) acc)) [] coll))
