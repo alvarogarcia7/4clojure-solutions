2d11068f1bb0459a3ba7c3c5e14a04f698de7571
Alvaro Garcia <derecerca@gmail.com>
2017-03-21 07:31:44 +0000

    Refactor: whitespace

--- src/solution/e56.clj
+++ src/solution/e56.clj
@@ -1,1 +1,8 @@
-(fn [coll] (reduce (fn [acc ele] (if (nil? (some #{ele} acc)) (conj acc ele) acc)) [] coll))
+(fn
+  [coll]
+  (reduce (fn
+            [acc ele]
+            (if (nil? (some #{ele} acc))
+              (conj acc ele)
+              acc))
+          [] coll))
